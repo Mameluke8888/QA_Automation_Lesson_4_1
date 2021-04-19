7f6f90df4f1a7b762f0acf835caa5f9f95a90757
Evgeny Abdulin <evgenyabdulin@Evgenys-Mac-mini.local>
2021-04-19 15:15:33 -0500

    lesson_4_1.py - docstrings added

--- lesson_4_1.py
+++ lesson_4_1.py
@@ -2,9 +2,8 @@
 
 
 # Modify this function to return a list of strings as defined above
-
-
 def list_benefits():
+    """Initializes a list of strings"""
     list_of_benefits = ["More organized code", "More readable code", "Easier code reuse",
                         "Allowing programmers to share and connect code together"]
     return list_of_benefits
@@ -12,12 +11,13 @@ def list_benefits():
 
 
 # Modify this function to concatenate to each benefit - " is a benefit of functions!"
-
 def build_sentence(benefit):
+    """Adds an extra string"""
     return benefit + " is a benefit of functions!"
 
 
 def name_the_benefits_of_functions():
+    """prints first 4 members of the list of strings"""
     list_of_benefits = list_benefits()
 
     print(build_sentence(list_of_benefits[0]))
